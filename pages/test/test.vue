95e7c86674183181d4360510f8d76c8fdb754409
henno01 <h.redenius@mdsi.de>
2024-01-25 09:28:30 +0100

    first commit

---
+++ pages/test/test.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const counter = ref(0)
+</script>
+    
+<template>
+    <button @click="counter--">-</button>
+    <span>{{ counter }}</span>
+    <button @click="counter++">+</button>
+    <span>Ahoi</span>
+</template>
