6b950162937e4cf76d9908b2e2914aaa7a7b3345
Hendrik Redenius <h.redenius@mdsi.de>
2024-02-15 10:01:12 +0100

    added type to device

---
+++ server/database/migrations/0001_known_wilson_fisk.sql
@@ -0,0 +1,1 @@
+ALTER TABLE device ADD `type` integer NOT NULL;
--- server/database/schemas/device.ts
+++ server/database/schemas/device.ts
@@ -1,10 +1,11 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const devices = sqliteTable("device", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
   ip: text("ip").unique(),
+  type: int("type").notNull()
 });
 
 export const insertDeviceSchema = createInsertSchema(devices);
