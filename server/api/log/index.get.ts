e0f4e54cdd110520437addb6bb5903a9fc3d0428
Julian <julian-pascal.ehlers@elektrosolid.de>
2024-02-22 10:40:58 +0100

    Eventlog query erweitert

--- server/api/log/index.get.ts
+++ server/api/log/index.get.ts
@@ -1,8 +1,9 @@
-import { SQL } from "drizzle-orm";
+import { SQL, getTableColumns } from "drizzle-orm";
 import { z } from "zod";
 import { SelectEvent } from "~/server/database/schemas/events";
 
 const querySchema = z.object({
+  device: z.string().optional(),
   to: z.coerce.date().optional(),
   from: z.coerce.date().optional(),
 });
@@ -24,10 +25,20 @@ export default defineEventHandler<
     filters.push(gte(events.timestamp, query.from));
   }
 
+  if (query.device) {
+    filters.push(eq(events.deviceId, query.device));
+  }
+
   const eventsQuery = db
-    .select()
+    .select({
+      ...getTableColumns(events),
+      deviceName: devices.name,
+      deviceType: devices.type
+    })
     .from(events)
+    .leftJoin(devices, eq(devices.id, events.deviceId))
     .where(and(...filters));
 
+  // console.log(eventsQuery.toSQL())
   return eventsQuery;
 });
