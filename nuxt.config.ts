dd8cb5425854030d704da7285e87cd0a8101538b
TiamoP23 <43099774+TiamoP23@users.noreply.github.com>
2024-03-13 23:21:50 +0000

    Inactive Geräte anpingen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,9 @@ export default defineNuxtConfig({
     typescript: {
       tsConfig,
     },
+    scheduledTasks: {
+      "* * * * *": ["device:ping"],
+    },
   },
   typescript: {
     tsConfig,
