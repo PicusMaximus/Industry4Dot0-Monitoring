5b275e234389dd645851a43a2d208a1091c587c6
Julian <julian-pascal.ehlers@elektrosolid.de>
2024-05-16 10:58:16 +0200

    Geräte Jobs angepasst

--- pages/device/[id].vue
+++ pages/device/[id].vue
@@ -85,7 +85,19 @@ useIntervalFn(refreshJobs, refreshInterval);
       />
     </DashboardSection>
 
-    <DashboardJobsSection />
+    <DashboardSection title="Verfügbare Jobs">
+      <div v-if="jobs?.length" class="grid gap-1 grid-cols-auto-fill-md">
+        <DashboardCard
+          v-for="(job, index) in jobs"
+          :key="index"
+          :name="job.name"
+          :rows="[{ title: 'Position', value: String(job?.order + 1) }]"
+        />
+      </div>
+      <div v-else class="flex justify-around">
+        <p>Keine aktiven Jobs</p>
+      </div>
+    </DashboardSection>
 
     <DashboardSection title="Ereignisse">
       <div class="flex flex-col gap-5">
